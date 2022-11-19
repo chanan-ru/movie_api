149982f8b091b9fe261b6b292743da1edf0efe86
chanan-ru <chananchida.ru@gmail.com>
2022-11-19 16:04:45 +1100

    Added modules and routes

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Test
+const express = require('express'),
+      morgan = require('morgan');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Movie app!!');
+  });
+
+// This will return a JSON object containing data about top 10 movies.
+app.get('/movies', (req, res) => {
+    res.send('Here is top 10 movies.');
+});
+
+
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+//   });
+
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
