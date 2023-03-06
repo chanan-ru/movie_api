c2a2985aaadaadf47e09ae6fa9afd18a23047d68
chanan-ru <chananchida.ru@gmail.com>
2023-03-06 22:26:57 +1100

    Update index.js

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ app.get('/', (req, res) => {
 
 // This will return all movies.
 //any request to the “movies” endpoint will require a JWT from the client.
-// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // app.get('/movies', (req, res) => {
   Movies.find()
     .populate('GenreID')
     .populate('DirectorID')
